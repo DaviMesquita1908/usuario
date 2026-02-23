a298a773843cd3ce52018e1ea3ecfdf592c50e7b
Davi <davimesquita1908@gmail.com>
2026-02-23 10:16:11 -0300

    adicionado o método que salva dados do usuário

---
+++ src/main/java/com/davimesquita/usuario/Business/Dto/TelefoneDTO.java
@@ -0,0 +1,16 @@
+package com.davimesquita.usuario.Business.Dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class TelefoneDTO {
+
+    private String numero;
+    private String ddd;
+
+}
--- src/main/java/com/davimesquita/usuario/infrastructure/Entity/Usuario.java
+++ src/main/java/com/davimesquita/usuario/infrastructure/Entity/Usuario.java
@@ -1,10 +1,7 @@
 package com.davimesquita.usuario.infrastructure.Entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,7 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table (name = "usuario")
-
+@Builder
 
 public class Usuario implements UserDetails {
 
